306a9c30629168e14c42c34449671c0922827266
Mateo Abascal <mateo@mateos-mbp-2.lan>
2020-11-06 12:41:33 -0500

    better dice roll

--- Rust/src/dice.rs
+++ Rust/src/dice.rs
@@ -1,8 +1,8 @@
-use rand::Rng;
+use rand::distributions::{Distribution, Uniform};
 
 pub fn roll() -> [u8; 2] {
 	let mut rng = rand::thread_rng();
-	let d1 = rng.gen_range(1,7);
-	let d2 = rng.gen_range(1,7);
-	return [d1, d2]
+	let d1 = Uniform::from(1..7);
+	let d2 = Uniform::from(1..7);
+	return [d1.sample(&mut rng), d2.sample(&mut rng)]
 }
